850079029b0fb2cfa2adfcda81ea08075e831afc
Einlanzerous <einlanzerous@live.com>
2017-12-18 18:33:30 -0500

    Trader changes.

--- test/Trader.java
+++ test/Trader.java
@@ -14,12 +14,12 @@ public class Trader extends Thread implements TradeScreen{
 	private HashMap<Integer,Order> orders=new HashMap<Integer,Order>();
 	private static Socket omConn;
 	private int port;
+	private ObjectOutputStream os;
 	Trader(String name,int port){
 		this.setName(name);
 		this.port=port;
 	}
-	ObjectInputStream  is;
-	ObjectOutputStream os;
+
 	public void run(){
 		//OM will connect to us
 		try {
@@ -29,14 +29,18 @@ public class Trader extends Thread implements TradeScreen{
 			InputStream s=omConn.getInputStream(); //if i try to create an objectinputstream before we have data it will block
 			while(true){
 				if(0<s.available()){
-					is=new ObjectInputStream(s);  //TODO check if we need to create each time. this will block if no data, but maybe we can still try to create it once instead of repeatedly
-					api method=(api)is.readObject();
+					ObjectInputStream is = new ObjectInputStream(s);
+					api method=(api) is.readObject();
 					System.out.println(Thread.currentThread().getName()+" calling: "+method);
 					switch(method){
-						case newOrder:newOrder(is.readInt(),(Order)is.readObject());break;
-						case price:price(is.readInt(),(Order)is.readObject());break;
-						case cross:is.readInt();is.readObject();break; //TODO
-						case fill:is.readInt();is.readObject();break; //TODO
+						case newOrder:newOrder(is.readInt(),(Order) is.readObject());break;
+						case price:price(is.readInt(),(Order) is.readObject());break;
+						case cross:
+							is.readInt();
+							is.readObject();break; //TODO
+						case fill:
+							is.readInt();
+							is.readObject();break; //TODO
 					}
 				}else{
 					//System.out.println("Trader Waiting for data to be available - sleep 1s");
