5c14fb775897631c4fbe786fbc66536b9f6a7fa9
jmubang <jmubang@gmail.com>
2017-12-18 17:18:51 -0500

    made public modifiers to private access modifiers to NewOrderSingle class and added getters. Utilized getters in OrderManager.

--- src/OrderClient/NewOrderSingle.java
+++ src/OrderClient/NewOrderSingle.java
@@ -5,13 +5,25 @@ import java.io.Serializable;
 import Ref.Instrument;
 
 public class NewOrderSingle implements Serializable{
-	public int size;
-	public float price;
-	public Instrument instrument;
+	private int size;
+	private float price;
+	private Instrument instrument;
 
 	public NewOrderSingle(int size,float price,Instrument instrument){
 		this.size=size;
 		this.price=price;
 		this.instrument=instrument;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public Instrument getInstrument() {
+		return instrument;
 	}
 }
