48e72a47688d9c96b3113023ab707df952e8334c
Einlanzerous <einlanzerous@live.com>
2017-12-19 13:50:51 -0500

    Cleaned up Sample Router

--- test/SampleRouter.java
+++ test/SampleRouter.java
@@ -10,30 +10,36 @@ import Ref.Ric;
 import javax.net.ServerSocketFactory;
 
 public class SampleRouter extends Thread implements Router{
-	private static final Random RANDOM_NUM_GENERATOR=new Random();
-	private static final Instrument[] INSTRUMENTS={new Instrument(new Ric("VOD.L")), new Instrument(new Ric("BP.L")), new Instrument(new Ric("BT.L"))};
+	private static final Random RANDOM_NUM_GENERATOR = new Random();
+	private static final Instrument[] INSTRUMENTS = {new Instrument(new Ric("VOD.L")), new Instrument(new Ric("BP.L")), new Instrument(new Ric("BT.L"))};
 	private Socket omConn;
 	private int port;
-	public SampleRouter(String name,int port){
+	private ObjectInputStream is;
+	private ObjectOutputStream os;
+
+	public SampleRouter(String name, int port){
 		this.setName(name);
-		this.port=port;
+		this.port = port;
 	}
-	ObjectInputStream is;
-	ObjectOutputStream os;
+
 	public void run(){
 		//OM will connect to us
 		try {
 			omConn= ServerSocketFactory.getDefault().createServerSocket(port).accept();
 			while(true){
-				if(0<omConn.getInputStream().available()){
+				if(0 < omConn.getInputStream().available()){
 					is=new ObjectInputStream(omConn.getInputStream());
 					Router.api methodName=(Router.api)is.readObject();
-					System.out.println("Order Router recieved method call for:"+methodName);
+					System.out.println("Order Router received method call for:" + methodName);
 					switch(methodName){
-						case routeOrder:routeOrder(is.readInt(),is.readInt(),is.readInt(),(Instrument)is.readObject());break;
-						case priceAtSize:priceAtSize(is.readInt(),is.readInt(),(Instrument)is.readObject(),is.readInt());break;
+						case routeOrder:
+							routeOrder(is.readInt(), is.readInt(), is.readInt(), (Instrument)is.readObject());
+							break;
+						case priceAtSize:
+							priceAtSize(is.readInt(), is.readInt(), (Instrument)is.readObject(), is.readInt());
+							break;
 					}
-				}else{
+				} else{
 					Thread.sleep(100);
 				}
 			}
@@ -43,12 +49,12 @@ public class SampleRouter extends Thread implements Router{
 		}
 	}
 	@Override
-	public void routeOrder(int id,int sliceId,int size,Instrument i) throws IOException, InterruptedException{
-		int fillSize=RANDOM_NUM_GENERATOR.nextInt(size);
+	public void routeOrder(int id, int sliceId, int size, Instrument i) throws IOException, InterruptedException {
+		int fillSize = RANDOM_NUM_GENERATOR.nextInt(size);
 		//TODO have this similar to the market price of the instrument
-		double fillPrice=199*RANDOM_NUM_GENERATOR.nextDouble();
+		double fillPrice = 199 * RANDOM_NUM_GENERATOR.nextDouble();
 		Thread.sleep(42);
-		os=new ObjectOutputStream(omConn.getOutputStream());
+		os = new ObjectOutputStream(omConn.getOutputStream());
 		os.writeObject("newFill");
 		os.writeInt(id);
 		os.writeInt(sliceId);
@@ -58,15 +64,15 @@ public class SampleRouter extends Thread implements Router{
 	}
 
 	@Override
-	public void sendCancel(int id,int sliceId,int size,Instrument i){
+	public void sendCancel(int id, int sliceId, int size, Instrument i){
 	}
 	@Override
-	public void priceAtSize(int id, int sliceId,Instrument i, int size) throws IOException{
-		os=new ObjectOutputStream(omConn.getOutputStream());
+	public void priceAtSize(int id, int sliceId, Instrument i, int size) throws IOException{
+		os = new ObjectOutputStream(omConn.getOutputStream());
 		os.writeObject("bestPrice");
 		os.writeInt(id);
 		os.writeInt(sliceId);
-		os.writeDouble(199*RANDOM_NUM_GENERATOR.nextDouble());
+		os.writeDouble(199 * RANDOM_NUM_GENERATOR.nextDouble());
 		os.flush();
 	}
 }
